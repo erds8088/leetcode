fe8ddda53cb134fe6e7e479bedab84928c85175f
xiongsyao <xiongshengyao@hotmail.com>
2018-11-21 16:40:38 +0800

    update solution for 46

--- 46_Permutations/xsy/solution.go
+++ 46_Permutations/xsy/solution.go
@@ -1,15 +1,16 @@
 func permute(nums []int) [][]int {
-    if len(nums) <= 1 {
+    lnums := len(nums)
+    if lnums <= 1 {
         return [][]int{nums}
     }
     result := [][]int{}
-    perms := permute(nums[1:])
+    perms := permute(nums[:lnums-1])
     for _, perm := range perms {
         end := len(perm)
         for i := 0; i<=end; i++ {
             p := []int{}
             p = append(p, perm[:i]...)
-            p = append(p, nums[0])
+            p = append(p, nums[lnums-1])
             p = append(p, perm[i:]...)
         result = append(result, p)
         }
--- 46_Permutations/xsy/solution.py
+++ 46_Permutations/xsy/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def permute(self, nums):
         """
-        可以转化成求nums[1:]的组合，然后将nums[0]插入到nums[1:]的所有组合中
+        可以转化成求nums[:-1]的组合，然后将nums[-1]插入到nums[:-1]的所有组合中
         :type nums: List[int]
         :rtype: List[List[int]]
         """
@@ -9,9 +9,9 @@ class Solution:
             return [nums]
         
         result = []
-        perms = self.permute(nums[1:])
+        perms = self.permute(nums[:-1])
         for perm in perms:
             for i in range(len(perm)+1):
-                p = perm[:i] + nums[:1] + perm[i:]
+                p = perm[:i] + nums[-1:] + perm[i:]
                 result.append(p)
         return result
